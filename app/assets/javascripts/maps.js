89f07f97de685f375e37e35375f3a72b7c9939c4
Ayat11 <ayatafy@gmail.com>
2017-11-20 16:02:22 +0100

    adding comments to map js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,9 +1,9 @@
-var lat = 52.53;
-var lng = 13.403;
+var latCenter = 52.53;
+var lngCenter = 13.403;
 var radius = 3500;
 var positionCenter;
 var vehicleIds = {};
-var markersRecord = {};
+var markersRecord = {}; 
 var markersMap = [];
 var svgPath;
 var map;
@@ -13,6 +13,7 @@ var markerCluster;
 function createCustomMarker(coords, map, title, oldPosition, newPosition) {
   var markerRotation = null;
 
+  // calculate rotation if 2 points exist.
   if (oldPosition && newPosition) {
     markerRotation = google.maps.geometry.spherical.computeHeading(oldPosition, newPosition);
   }
@@ -65,10 +66,12 @@ function clearMarkers() {
   markersMap = [];
 }
 
-function outOfBoundaries(position) {
+// return true if the distance between two points exceed 3.5km (city boundaries)
+function isOutOfBoundaries(position) {
   return (google.maps.geometry.spherical.computeDistanceBetween(positionCenter, position) > radius);
 }
 
+// this method removes the markers on the map and renders them with the updated position
 function updateMarkers() {
   clearMarkers();
   $.each(vehicleIds, function (id, location) {
@@ -78,7 +81,7 @@ function updateMarkers() {
     }
 
     var position = new google.maps.LatLng(location['lat'], location['lng']);
-    if (!outOfBoundaries(position)) {
+    if (!isOutOfBoundaries(position)) {
       var marker;
       if (previousMarker) {
         marker = createCustomMarker(
@@ -102,7 +105,7 @@ function updateMarkers() {
   });
 }
 
-function hitDatabase() {
+function getVehicleUpdates() {
   $.ajax({
     type: 'GET',
     url: '/get_vehicles_updates',
@@ -131,11 +134,10 @@ function initSVGPath() {
 
 $(document).ready(function () {
   initSVGPath();
-  positionCenter = new google.maps.LatLng(lat, lng);
+  positionCenter = new google.maps.LatLng(latCenter, lngCenter);
   initMap();
   markerCluster = new MarkerClusterer(map, markersMap, { imagePath: '../assets/cluster/m' });
 
-  // need to hit the database every 5 seconds and get the new registered vehicles as well as the updated positions for each vehicle
-
-  setInterval(hitDatabase, 3000);
+  // we need to hit the database every 3 seconds and get the new registered vehicles as well as the updated positions for each vehicle
+  setInterval(getVehicleUpdates, 3000);
 });
